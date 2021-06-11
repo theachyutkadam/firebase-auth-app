aa7cdd792c0a5a6a9105f9247f9024679a9db6f2
achyut kadam <theachyutkadam@gmail.com>
2021-06-11 16:35:05 +0530

    completed upto leasson 9

---
+++ scripts/auth.js
@@ -0,0 +1,59 @@
+// guides table data
+db.collection('guides').get().then(snapShot => {
+  setupGuides(snapShot.docs);
+  console.log(snapShot.docs);
+})
+
+// signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  // sign up the user
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred.user);
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+
+    signupForm.reset();
+  });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut();
+})
+
+// login form
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // sign up the user
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    // close the login modal & reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+});
+
+// auth status change.
+auth.onAuthStateChanged(user => {
+  if (user){
+    console.log('user logged in!', user)
+  }else{
+    console.log('user logged out!')
+  }
+})
